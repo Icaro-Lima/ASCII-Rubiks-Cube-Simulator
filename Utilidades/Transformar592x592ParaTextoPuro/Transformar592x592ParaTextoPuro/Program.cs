5f2492e39f0884f5e118e86ecf5a2f0c4d5f3e69
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-05-15 05:00:11 -0300

    Adicionadas as animações em texto

---
+++ Utilidades/Transformar592x592ParaTextoPuro/Transformar592x592ParaTextoPuro/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace Transformar592x592ParaTextoPuro
+{
+    class Program
+    {
+        static int Mean(Bitmap image, int i, int j, int cols, int rows)
+        {
+            int sum = 0, count = 0;
+            for (int ii = 0; ii < rows; ii++)
+            {
+                for (int jj = 0; jj < cols; jj++)
+                {
+                    if (i + ii >= image.Height || j + jj >= image.Width)
+                    {
+                        continue;
+                    }
+
+                    count++;
+                    Color color = image.GetPixel(j + jj, i + ii);
+                    sum += (color.R + color.G + color.B) / 3;
+                }
+            }
+
+            return sum / count;
+        }
+
+        static void Process(string path)
+        {
+            const int blockWidth = 8;
+            const int blockHeight = 16;
+
+            Bitmap bitmap = (Bitmap)Image.FromFile(path);
+
+
+            string concat = "";
+            string[][] result = new string[bitmap.Height / 16][];
+            for (int i = 0; i < bitmap.Height; i += blockHeight)
+            {
+                for (int j = 0; j < bitmap.Width; j += blockWidth)
+                {
+                    concat += Mean(bitmap, i, j, blockWidth, blockHeight) == 255 ? " " : "#";
+                }
+                concat += "\n";
+            }
+
+            using (StreamWriter sw = new StreamWriter(Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path) + ".txt"), false))
+            {
+                sw.Write(concat);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o caminho: ");
+            string path = @"C:\Users\Lenovo\Documents\Unity\Apenas visualizando o cubo";
+            string[] paths = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
+            for (int i = 0; i < paths.Length; i++)
+            {
+                Process(paths[i]);
+            }
+        }
+    }
+}
---
+++ Utilidades/Transformar592x592ParaTextoPuro/Transformar592x592ParaTextoPuro/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Transformar592x592ParaTextoPuro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Transformar592x592ParaTextoPuro")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97d1d80b-542b-4050-9017-d61ffab96a3a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
