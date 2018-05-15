a1b8bf7d8af1f63ecbccb510835a2069c6e07c96
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-05-15 02:42:56 -0300

    Adicionado pasta utilidades, bem como um utilitário para transformar as imagens para 592x592.

---
+++ Utilidades/CroparCuboPara592/CroparCuboPara592/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace CroparCupoPara592
+{
+    class Program
+    {
+        static void Process(string path)
+        {
+            Bitmap original = null;
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                original = (Bitmap)Image.FromStream(stream);
+            }
+            Bitmap newBitmap = new Bitmap(592, 592);
+
+            using (Graphics graphics = Graphics.FromImage(newBitmap))
+            {
+                graphics.DrawImageUnscaled(original, -original.Width / 2 + newBitmap.Width / 2, -original.Height / 2 + newBitmap.Height / 2);
+            }
+
+            File.Delete(path);
+
+            newBitmap.Save(path);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o caminho: ");
+            string path = Console.ReadLine().Replace("\"", "");
+            string[] paths = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
+            Console.WriteLine(paths.Length);
+            for (int i = 0; i < paths.Length; i++)
+            {
+                Process(paths[i]);
+            }
+        }
+    }
+}
