ed2bcf1cc94a32a21001569482b2fe7f063c1f71
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-05-17 01:21:52 -0300

    Adicionado utilitário do Unity e mais animações

---
+++ Utilidades/Apenas visualizando o cubo/Assets/Print.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Print : MonoBehaviour
+{
+
+    public int id = 0;
+    public string path = "0Right";
+    public int diff = 15;
+
+    public GameObject cube;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (System.IO.Directory.Exists(path))
+            {
+                Debug.Log("HEY MEEE!!");
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory("Cubo/" + path);
+            }
+
+            ScreenCapture.CaptureScreenshot("Cubo/" + path +"/" + id++ + "_mask.png");
+            cube.transform.Rotate(new Vector3(1, 0, 0), diff);
+        }
+
+    }
+}
