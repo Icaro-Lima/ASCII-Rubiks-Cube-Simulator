a7570c00591e40ec9b1b766e54cf24d80363fa0a
igoratf <igor.atf@outlook.com>
2018-07-03 21:43:35 -0300

    add unformatted menu

--- Haskell/api.hs
+++ Haskell/api.hs
@@ -1,1 +1,11 @@
 
+draw_menu = do
+    putStrLn "-----------------------  RUBIK CUBE SIMULATOR -----------------------"
+    putStrLn "Pressione I para Instruções"
+    putStrLn "Pressione J para Jogar"
+    putStrLn "Pressione ESC para Sair"
+    putStrLn "Icaro Dantas, Igor Farias, Javan Lacerda, Lucas Araújo, Sérgio Duarte"
+
+main :: IO()
+main = do
+    draw_menu
