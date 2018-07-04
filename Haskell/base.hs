a9e906a41507e3237d2c56bd9753c290fcb82ad0
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-07-04 05:34:51 -0300

    adiciona animação da Logo no base.hs -> loadAnimations

--- Haskell/base.hs
+++ Haskell/base.hs
@@ -7,6 +7,7 @@ import System.IO.Unsafe (unsafeDupablePerformIO)
 assets_path = "../Assets"
 line_limit = 500
 
+-- Método auxiliar de 
 getFileLines :: String -> [String]
 getFileLines path = do
   let file = unsafeDupablePerformIO (readFile path)
@@ -19,9 +20,10 @@ loadAnimations = do
   Map.fromList ([(movement ++ "_" ++ idx, getFileLines (cube_path ++ "/" ++ movement ++ "/" ++ idx ++ ".txt")) 
     | movement <- [a ++ b | a <- ["0","1","2"], b <- ["Left","Right"]] ++ [a ++ b | a <- ["A","B","C"], b <- ["Up","Down"]] ++ [a ++ b | a <- ["a","b","c"],
     b <- ["Clockwise","Counterclockwise"]],
-    idx <- ["0","1","2","3","4"]] ++ [("Default",getFileLines (cube_path ++ "/Default.txt"))])
+    idx <- ["0","1","2","3","4"]] ++ [("Default",getFileLines (cube_path ++ "/Default.txt"))] ++ 
+    [("Logo_" ++ idx,getFileLines (assets_path ++ "/Logo/" ++ idx ++ ".txt")) | idx <- map (show) [0..43]])
 
 main :: IO()
 main = do
-  mapM_ (putStrLn) (fromMaybe [""] (Map.lookup "Default" (loadAnimations)))
+  mapM_ (putStrLn) (fromMaybe [""] (Map.lookup "Logo_0" (loadAnimations)))
   print "Let's go!"
