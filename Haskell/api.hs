d370d297b55b60abbeb1a71774dac65e844c41d5
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-07-04 21:42:59 -0300

    adiciona 'writeText' em 'base.hs'

--- Haskell/api.hs
+++ Haskell/api.hs
@@ -1,1 +1,17 @@
+import Data.Maybe (fromMaybe)
+import qualified Base as Base
+import qualified Data.Map.Strict as Map
 
+{-|
+  Escreve texto a partir de uma determinada posição.
+  Int :      Posição i na matriz.
+  Int :      Posição j na matriz.
+  String :   Texto a ser escrito.
+  [String] : Estado atual da matriz.
+-}
+writeText :: Int -> Int -> String -> [String] -> [String]
+writeText i j text matrix = Base.writeTextAux i 0 j text matrix
+
+main :: IO()
+main = do
+  print (writeText 3 10 "Icaro" (fromMaybe [""] (Map.lookup "0Left_0" Base.loadAnimations)))
