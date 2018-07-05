55d2ae8472daff97cd9b0b13cf29dfd26dfc3320
igoratf <igor.atf@gmail.com>
2018-07-05 14:20:25 -0300

    add matrix getters

--- Haskell/MovimentosLogicos.hs
+++ Haskell/MovimentosLogicos.hs
@@ -1,1 +1,15 @@
+-- |Auxiliar!
+getMatrixLine :: Int -> Int -> Int -> Int -> [[Int]] -> [Int]
+getMatrixLine linha a b c cubeMatrix = do
+  let line = genericIndex cubeMatrix linha
+  
+  [genericIndex line a] ++ 
+    [genericIndex line b] ++ 
+    [genericIndex line c]
 
+-- |Auxiliar!
+getMatrixCol :: Int -> Int -> Int -> Int -> [[Int]] -> [Int]
+getMatrixCol coluna a b c cubeMatrix = 
+  [genericIndex (genericIndex cubeMatrix a) coluna] ++ 
+  [genericIndex (genericIndex cubeMatrix b) coluna] ++ 
+  [genericIndex (genericIndex cubeMatrix c) coluna]
