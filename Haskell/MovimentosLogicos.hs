e7ca344c5ee7f666c40a5729db1211b4a89d0e3e
igoratf <igor.atf@gmail.com>
2018-07-05 15:13:56 -0300

    add A down rotation

--- Haskell/MovimentosLogicos.hs
+++ Haskell/MovimentosLogicos.hs
@@ -206,6 +206,19 @@ doisDir cubeMatrix =
 
     giraFaceHorario 6 3 8 5 cubeMatrix
 
+aBaixo :: [[Int]] -> [[Int]]
+aBaixo cubeMatrix = 
+    let a = getMatrixCol 3 0 1 2 cubeMatrix
+        b = getMatrixCol 3 3 4 5 cubeMatrix
+        c = getMatrixCol 3 6 7 8 cubeMatrix
+        d = getMatrixCol 3 9 10 11 cubeMatrix
+    
+    swapC 3 0 2 d cubeMatrix
+    swapC 3 3 5 a cubeMatrix
+    swapC 3 6 8 b cubeMatrix
+    swapC 3 9 11 c cubeMatrix
+
+    giraFaceHorario 3 0 5 2 cubeMatrix
 
 
 
