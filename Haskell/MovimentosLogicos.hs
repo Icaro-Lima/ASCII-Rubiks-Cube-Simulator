83a63abdb40d48b64dbfef5e5999db8172c613cf
igoratf <igor.atf@gmail.com>
2018-07-05 14:30:01 -0300

    add clockwise rotation draft

--- Haskell/MovimentosLogicos.hs
+++ Haskell/MovimentosLogicos.hs
@@ -13,3 +13,17 @@ getMatrixCol coluna a b c cubeMatrix =
   [genericIndex (genericIndex cubeMatrix a) coluna] ++ 
   [genericIndex (genericIndex cubeMatrix b) coluna] ++ 
   [genericIndex (genericIndex cubeMatrix c) coluna]
+
+
+giraFaceAntiHorario :: Int -> Int -> Int -> Int -> [[Int]] -> [Int]
+giraFaceAntiHorario lIni cIni lFim cFim = 
+    let a = getMatrixLine lIni cFim cFim-1 cIni cubeMatrix
+        b = getMatrixCol cFim lIni lFim-1 lFim cubeMatrix
+        c = getMatrixLine lFim cFim cFim-1 cIni cubeMatrix
+        d = getMatrixCol cIni lIni lFim-1 lFim
+
+    swapL lIni cIni cFim b cubeMatrix
+    swapC cFim lIni lFim c cubeMatrix
+    swapL lFim cIni cFim d cubeMatrix
+    swapC cIni lIni cFim a cubeMatrix
+
