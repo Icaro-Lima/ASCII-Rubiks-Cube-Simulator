eea6f685c7039d65fa8de9991e9265b0ac9930fa
igoratf <igor.atf@gmail.com>
2018-07-05 14:40:21 -0300

    add A counter-clockwise rotation

--- Haskell/MovimentosLogicos.hs
+++ Haskell/MovimentosLogicos.hs
@@ -16,7 +16,7 @@ getMatrixCol coluna a b c cubeMatrix =
 
 
 giraFaceAntiHorario :: Int -> Int -> Int -> Int -> [[Int]] -> [Int]
-giraFaceAntiHorario lIni cIni lFim cFim = 
+giraFaceAntiHorario lIni cIni lFim cFim cubeMatrix = 
     let a = getMatrixLine lIni cFim cFim-1 cIni cubeMatrix
         b = getMatrixCol cFim lIni lFim-1 lFim cubeMatrix
         c = getMatrixLine lFim cFim cFim-1 cIni cubeMatrix
@@ -28,15 +28,33 @@ giraFaceAntiHorario lIni cIni lFim cFim =
     swapC cIni lIni cFim a cubeMatrix
 
 giraFaceHorario :: Int -> Int -> Int -> Int -> [[Int]] -> [Int]
-giraFaceHorario lIni cIni lFim cFim =
+giraFaceHorario lIni cIni lFim cFim cubeMatrix =
     let a = getMatrixLine lIni cIni cFim-1 cFim cubeMatrix
         b = getMatrixCol cFim lFim lFim-1 lIni cubeMatrix
         c = getMatrixLine lFim cIni cFim-1 cFim cubeMatrix
         d = getMatrixCol cIni lFim lFim-1 lIni cubeMatrix
 
-    swapL lIni cIni cFim d
-    swapC cIni lIni lFim c
-    swapL lFim cIni cFim b
-    swapC cFim lIni lFim a
+    swapL lIni cIni cFim d cubeMatrix
+    swapC cIni lIni lFim c cubeMatrix
+    swapL lFim cIni cFim b cubeMatrix
+    swapC cFim lIni lFim a cubeMatrix
+
+aAntiHorario :: [[Int]] -> [Int]
+aAntiHorario cubeMatrix = 
+    let a = getMatrixLine 2 5 4 3 cubeMatrix
+        b = getMatrixCol 6 3 4 5 cubeMatrix
+        c = getMatrixLine 6 5 4 3 cubeMatrix
+        d = getMatrixCol 2 3 4 5 cubeMatrix
+
+
+    swapL 2 3 5 b cubeMatrix
+    swapC 6 3 5 c cubeMatrix
+    swapL 6 3 5 d cubeMatrix
+    swapC 2 3 5 a cubeMatrix
+
+    giraFaceAntiHorario 3 3 5 5 cubeMatrix
+
+
+
 
 
