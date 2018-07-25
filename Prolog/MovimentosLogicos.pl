e7667e02b250b840aff0b55db462893819814c51
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-07-25 13:17:18 -0300

    implementa 'swapL' e 'swapC'

--- Prolog/MovimentosLogicos.pl
+++ Prolog/MovimentosLogicos.pl
@@ -19,3 +19,39 @@ replace(0, Element, [_|Tail], [Element|Tail]).
 replace(Index, Element, [Head|Tail], [Head|TailOut]) :-
 	N is Index - 1,
 	replace(N, Element, Tail, TailOut).
+
+swapL(0, Begin, End, Array, [Head|Tail], [HeadOut|Tail]) :-
+	nth0(0, Array, A),
+	nth0(1, Array, B),
+	nth0(2, Array, C),
+
+	replace(Begin, A, Head, AA),
+	replace(Begin + 1, B, AA, BB),
+	replace(End, C, BB, HeadOut).
+
+swapL(I, Begin, End, Array, [Head|Tail], [Head|TailOut]) :-
+	II is I - 1,
+	swapL(II, Begin, End, Array, Tail, TailOut).
+
+swapC(J, Begin, End, Array, MatrixIn, MatrixOut) :-
+	nth0(0, Array, Array0),
+	nth0(1, Array, Array1),
+	nth0(2, Array, Array2),
+
+	Beg1 is Begin + 1,
+
+	nth0(Begin, MatrixIn, LineA),
+	nth0(Beg1, MatrixIn, LineB),
+	nth0(End, MatrixIn, LineC),
+
+	replace(J, Array0, LineA, LineAA),
+	replace(J, Array1, LineB, LineBB),
+	replace(J, Array2, LineC, LineCC),
+
+	replace(Begin, LineAA, MatrixIn, AA),
+	replace(Beg1, LineBB, AA, BB),
+	replace(End, LineCC, BB, MatrixOut).
+
+test :-
+	cubeMatrix(Matrix), swapC(0, 9, 11, [-1, -2, -3], Matrix, Out),
+	writeln(Out).
